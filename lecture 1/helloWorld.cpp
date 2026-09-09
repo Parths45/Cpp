27a30326cf936cad59fc4d7b02961a5cde6113b1
Parths45 <shastriparth45@gmail.com>
2026-09-09 11:21:10 +0530

    lecture 1 commit

---
+++ lecture 1/helloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    // this is a single line comment in C++ like in python
+
+    /*
+    This is 
+    a multi-line comment in C++
+    */
+   
+    std::cout << "Hello, World" << '\n'; // \n is used for new line like in python 
+
+    std::cout << "This is been written in CPP" << std::endl ; // std::endl is also used for new line but it also flushes the output buffer
+
+    return 0;
+}
